d93f0a4994c61874cf67856f7868bb4447245ea2
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-28 17:16:29 +0530

    Updates

---
+++ precache-manifest.905aee3969540bbd0b8d5fa03a4f5878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba994634f9bf90d1090ac213c6d94dae",
+    "url": "/tabs-react/index.html"
+  },
+  {
+    "revision": "5940075750ae7dfde1b9",
+    "url": "/tabs-react/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tabs-react/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs-react/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5940075750ae7dfde1b9",
+    "url": "/tabs-react/static/js/main.882ec7e8.chunk.js"
+  },
+  {
+    "revision": "5465f10596d2b4996e99",
+    "url": "/tabs-react/static/js/runtime-main.ad4106c1.js"
+  }
+]);
